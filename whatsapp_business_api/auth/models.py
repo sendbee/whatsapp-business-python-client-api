6275771522972dfba775c87299c1411e10723bcc
Ivan Arar <ivan.arar@gmail.com>
2024-01-31 12:48:59 +0100

    refresh auth token

--- whatsapp_business_api/auth/models.py
+++ whatsapp_business_api/auth/models.py
@@ -6,3 +6,6 @@ class OauthToken(Model):
     """Data model for oauth token response"""
 
     _token = TextField(index='access_token', desc='oAuth access token')
+    _token_type = TextField(index='token_type', desc='bearer')
+    _expires_in = TextField(index='expires_in',
+                            desc='Time left in seconds until the token expires')
--- whatsapp_business_api/auth/query_params.py
+++ whatsapp_business_api/auth/query_params.py
@@ -8,3 +8,8 @@ class OauthToken(QueryParams):
     client_secret = 'client_secret', 'Facebook app secret'
     redirect_uri = 'redirect_uri', 'oAuth redirect ID'
     code = 'code', 'oAuth code'
+    grant_type = 'grant_type', 'authorization_code'
+    fb_exchange_token = 'fb_exchange_token', 'A valid system user access token'
+    set_token_expires_in_60_days = \
+        'set_token_expires_in_60_days', \
+            'set to true to refresh an expiring system user access token'
